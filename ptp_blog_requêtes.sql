61dc6a0b834c974eb1b5105bafdd27ac10c941d2
Ludovic Cellard <ludovic.cellard@free.fr>
2016-04-11 17:11:38 +0200

    Ajout requête page catégorie

--- ptp_blog_requêtes.sql
+++ ptp_blog_requêtes.sql
@@ -21,3 +21,17 @@ FROM article
 WHERE utilisateur_id=1
 ORDER BY dT_edit
 
+-- ------------------------------------------------------------
+--  page catégorie : articles d’une catégorie, également triés par date
+-- ------------------------------------------------------------
+
+SELECT article.titre, id, DATE_FORMAT(article.dT_edit, '%d/%m/%Y') AS dt_art, article.extrait, texte
+FROM article 
+INNER JOIN categorie_article 
+	ON categorie_article.article_id=article.id
+WHERE categorie_article.categorie_id=1
+ORDER BY dT_edit
+
+
+
+
